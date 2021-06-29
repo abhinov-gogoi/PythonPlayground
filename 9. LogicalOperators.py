ee3d9963d0a1fe9830336ad84299f42322f0047e
abhinov <abhinov.in@gmail.com>
2021-06-29 11:44:47 +0530

    logical operators

---
+++ 9. LogicalOperators.py
@@ -0,0 +1,27 @@
+# LOGICAL or OPERATOR
+has_high_income = False
+has_good_credit = True
+
+if has_high_income or has_good_credit:
+    print("Eligible for Loan")
+else:
+    print("Not Eligible for Loan")
+
+# LOGICAL or OPERATOR
+is_adult = False
+is_healthy = True
+
+if is_adult and is_healthy:
+    print("Eligible for Vaccine")
+else:
+    print("NOT Eligible for Vaccine")
+
+# LOGICAL not OPERATOR (! in Java)
+has_good_credit = True
+has_criminal_record = False
+
+if has_good_credit and not has_criminal_record:
+    print("Eligible for Admission")
+else:
+    print("NOT Eligible for Admission")
+
