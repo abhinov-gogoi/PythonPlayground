b92b69ed92a907452c37f1a1067bf723f4f6b7a9
abhinov <abhinov.in@gmail.com>
2021-06-29 09:22:33 +0530

    formatted strings in python

---
+++ formattedString.py
@@ -0,0 +1,9 @@
+first_name = "John"
+last_name = "Smith"
+
+message = "Mr. " + first_name + " [" + last_name + "] is a coder"
+print(message)
+
+# Formatted String
+messageFormatted = f"Mr. {first_name} [{last_name}] is a coder"
+print(messageFormatted)
