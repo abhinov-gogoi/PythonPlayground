0fb2b899f603936c016a7baae0e07ddcc4f77d74
abhinov <abhinov.in@gmail.com>
2021-06-29 09:01:57 +0530

    init a git repo

---
+++ input.py
@@ -0,0 +1,19 @@
+# Print to the console
+print("Welcome to my first python program")
+
+# Take input from user
+name = input("What is your name? ")
+print("Hello " + name)
+
+# Type conversion / casting
+age = input("What is your age? ")
+birth_year = 2021 - int(age)
+print("You were born in " + str(birth_year))
+
+# Other type conversions
+int()
+float()
+bool()
+
+# Check variable type
+print(type(birth_year))
---
+++ string.py
@@ -0,0 +1,10 @@
+string = "Hello human, I love python programming"
+
+# String indexing
+print(string[0])
+print(string[-7])
+print(string[0:5])
+print(string[6:])
+print(string[:26])
+print(string[:])
+print(string[3:-3])
